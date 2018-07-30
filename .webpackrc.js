6ee77b942e1a75d2375b7eea96650357ddf3b809
Hsdiana <2831103860@qq.com>
2018-07-30 17:41:11 +0800

    登录

--- .webpackrc.js
+++ .webpackrc.js
@@ -22,6 +22,6 @@ export default {
     }
   },
   extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }]],
-  publicPath: '/',
-  // publicPath: 'http://popularize.9-a-m.com/west/',
+  // publicPath: '/',
+  publicPath: 'http://popularize.9-a-m.com/west/',
 };
--- src/routes/page/login/login.js
+++ src/routes/page/login/login.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import G from './../../../gobal';
 
+import Loginbox from './components/login'
+
 
 export default class Login extends Component {
-  
 
-  render() {
-    return (
-      <div>
-        <h2>Login</h2>
-      </div>
-    );
-  }
+
+    render() {
+        const { products } = this.props;
+        return (
+            <div>
+                <h2>List of Products</h2>
+                <Loginbox />
+            </div>
+        );
+    }
 }
